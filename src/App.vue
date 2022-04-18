fd097ef3c9be40bb9ac61fd4d460f323809ce719
A2v10 <hey.a2v10@gmail.com>
2022-04-18 22:46:33 +0530

    Initial Push

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,76 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main">
+    <div class="sidebar">
+      <div class="sidebar-header">
+        <MenuIcon
+          style="margin-left: 25px; margin-right: 25px"
+          fillColor="#FFFFFF"
+        />
+
+        <img
+          class="logo"
+          src="./assets/yt_logo_rgb_dark.png"
+          alt=""
+          srcset=""
+        />
+      </div>
+      <div class="nav-items-1">
+        <button>Home</button>
+        <button>Explore</button>
+        <button>Shorts</button>
+        <button>Subscriptions</button>
+      </div>
+    </div>
+    <div class="body"></div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MenuIcon from "vue-material-design-icons/Menu.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MenuIcon,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+}
+.main {
+  display: flex;
+}
+.sidebar {
+  width: 15vw;
+  height: 100vh;
+  background-color: #212121;
+}
+.sidebar-header {
+  display: flex;
+  margin-top: 20px;
+  justify-content: start;
+  align-items: flex-start;
+}
+.body {
+  width: 85vw;
+  height: 100vh;
+  background-color: #1e252c;
+}
+.logo {
+  height: 1.25rem;
+}
+.nav-items-1{
+  display: flex;
+  flex-direction: column;
+}
+.nav-items-1 button {
+  padding: 10px;
+  background: transparent;
+  color: white;
+  border: none;
 }
 </style>
